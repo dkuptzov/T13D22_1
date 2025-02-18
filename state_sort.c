073d61d71d0f64c676d196b0da165124e7472bce
dkuptzov <101925838+dkuptzov@users.noreply.github.com>
2025-02-18 19:44:00 +0300

    Update state_sort.c
    last 001

--- state_sort.c
+++ state_sort.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 //void open_file(FILE *file);
 void menu0(FILE *file, char *file_name);
+int compare(const void *a, const void *b);
 
-struct st{
+struct MyStruct{
     int year;
     int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+    int status;
+    int code;
 };
 
 int main()
@@ -41,37 +48,54 @@ int main()
 
 void menu0(FILE *file, char *file_name)
 {
-    char *file_in_memory;
+    //char *file_in_memory;
+    struct MyStruct data;
     file = fopen(file_name, "rb");
     if (file == NULL)
 	{
 		printf("n/a\n");
 		return;
 	}
+    //int n = sizeof(data) / sizeof(data[0]);
+
+    qsort(&data, 100, sizeof(struct MyStruct), compare);
+
+    while (fread(&data, sizeof(struct MyStruct), 1, file) == 1) {
+        printf("%d %d %d %d %d %d %d %d\n", data.year, data.month, data.day, data.hour, data.minute, 
+        data.second, data.status, data.code);
+    }
+    //printf("%d", data.year);
 
 
-    
-    fseek(file, 0, SEEK_END);
-    long file_size = ftell(file);
-    rewind(file);
-    file_in_memory = (char *)malloc(file_size);
-    if (file_in_memory == NULL)
-    {
-		printf("n/a\n");
-		return;
-    }
-    size_t result = fread(file_in_memory, 1, file_size, file);
-    if (result != (size_t)file_size)
-    {
-        printf("n/a\n");
-        free(file_in_memory);
-        fclose(file);
-        return;
-    }
-    for (int i = 0; i < file_size; i++)
-    {
-        printf("%c", file_in_memory[i]);
-    }
-    free(file_in_memory);
+    // fseek(file, 0, SEEK_END);
+    // long file_size = ftell(file);
+    // rewind(file);
+    // file_in_memory = (char *)malloc(file_size);
+    // if (file_in_memory == NULL)
+    // {
+	// 	printf("n/a\n");
+	// 	return;
+    // }
+    // size_t result = fread(file_in_memory, 1, file_size, file);
+    // if (result != (size_t)file_size)
+    // {
+    //     printf("n/a\n");
+    //     free(file_in_memory);
+    //     fclose(file);
+    //     return;
+    // }
+    // for (int i = 0; i < file_size; i++)
+    // {
+    //     printf("%c", file_in_memory[i]);
+    // }
+    // free(file_in_memory);
     fclose(file);
+}
+
+int compare(const void *a, const void *b) {
+    const struct MyStruct *data1 = (const struct MyStruct *)a;
+    const struct MyStruct *data2 = (const struct MyStruct *)b;
+
+    // Сравнение по полю year
+    return (data1->year - data2->year);
 }
